8da15c3f06bb8106c62c53fbf0723078b8001395
Qing Wang <kingchin1218@gmail.com>
2023-12-07 09:31:09 +0800

    Revert "merge" (#2)

--- base_station_env.py
+++ base_station_env.py
@@ -3,9 +3,6 @@ from gym import spaces
 from gym.utils import seeding
 from gym.spaces import Dict, Discrete
 import numpy as np
-import function as function
-import LSTM as LSTM
-import math
 
 
 gym.envs.register(
@@ -15,38 +12,6 @@ gym.envs.register(
 
 class BaseStationEnv(gym.Env):
     def __init__(self):
-        self.time = 0
-        self.done_flag = True  # 用于判断是否为合法情况
-        self.time = 0  # 当前时间，每循环一次time+1，（单位：ms）
-        self.wait_packetSize0 = 0  # 初始化三个基站的等待数据
-        self.wait_packetSize1 = 0
-        self.wait_packetSize2 = 0
-
-        self.BSmodel0 = 0  # 初始化三个BS的 work model
-        self.BSmodel1 = 0
-        self.BSmodel2 = 0
-
-        self.energy_sum = 0  # 初始化 三个BS的总能耗
-
-        self.transiotion_time_ratio0 = 0  # 初始化 三个BS的在1ms内用于转化work model所用时间占总1ms的比例：workmodel转换时间/1ms
-        self.transiotion_time_ratio1 = 0
-        self.transiotion_time_ratio2 = 0
-
-        self.transition_time0 = 0  # 初始化三个BS work model的转换时间，（单位ms）
-        self.transition_time1 = 0
-        self.transition_time2 = 0
-
-        self.SM_Hold_Time0 = 0  # 初始化每个基站在某个work model的持续时间
-        self.SM_Hold_Time1 = 0
-        self.SM_Hold_Time2 = 0
-
-        self.LSTM_user1 = 0  # 初始化LSTM
-        self.LSTM_user2 = 0
-        self.LSTM_user3 = 0
-        self.LSTM_user4 = 0
-        self.LSTM_user5 = 0
-
-
         # State_space:
             # time
             # BS0Model，BS1Model，BS2Model，
@@ -78,43 +43,18 @@ class BaseStationEnv(gym.Env):
 
     def step(self, action):
         # 执行给定的动作，并返回新的观测、奖励、是否终止和其他信息
-        while(done_flag):
-            if (Vars.start_trans_no0 == max(len(user1_data),len(user2_data),len(user3_data),len(user4_data),len(user5_data))) and wait_packetSize0 == 0 and wait_packetSize1 == 0 and wait_packetSize2 == 0:
-                done_flag = False
-            elif time >= max(len(user1_data),len(user2_data),len(user3_data),len(user4_data),len(user5_data)) + 800:
-                done_flag = False
-            else:
-                arrival_data_matrix0, arrival_data_matrix1, arrival_data_matrix2 =function.arrival_data_matrix(self.time,user1_data,user2_data,user3_data,user4_data,user5_data,action,self.arrival_data_matrix0,self.arrival_data_matrix1,self.arrival_data_matrix2)
-                wait_packetSize0, wait_packetSize1, wait_packetSize2 = function.wait_packetSize(time,arrival_data_matrix0,arrival_data_matrix1,arrival_data_matrix2,waitpacketSize0,wait_packetSize1,wait_packetSize2)
-                h_15, trans_rate_list_user1, trans_rate_list_user2, trans_rate_list_user3, trans_rate_list_user4, trans_rate_list_user5 = function.trans_rate_15(action)
-                if self.transition_time0 == 0: #处于非转换时间
-                    transiotion_time_ratio0, transiotion_time_ratio1, transiotion_time_ratio2, energy_sum, arrival_data_matrix0, arrival_data_matrix1, arrival_data_matrix2 = function.BS_power(time, action,BSmodel0,BSmodel1,BSmodel2, trans_rate0,transiotion_time_ratio0,trans_rate1,transiotion_time_ratio1,trans_rate2,transiotion_time_ratio2,wait_packetSize0, wait_packetSize1, wait_packetSize2,arrival_data_matrix0,arrival_data_matrix1,arrival_data_matrix2,energy_sum,transition_time0,transition_time1,transition_time2)
-                    BSmodel0, BSmodel1, BSmodel2, energy_sum, transiotion_time_ratio0, transiotion_time_ratio1, transiotion_time_ratio2, transition_time0, transition_time1, transition_time2, mode_time0, mode_time1, mode_time2 = function.transition(BSmodel0,BSmodel1,BSmodel2,action,energy_sum,transiotion_time_ratio0,transiotion_time_ratio1,transiotion_time_ratio2,transition_time0,transition_time1,transition_time2,Vars.mode_time0,Vars.mode_time1,Vars.mode_time2)
-                    Vars.mode_time0 = mode_time0
-                    Vars.mode_time1 = mode_time1
-                    Vars.mode_time2 = mode_time2
-                    LSTM_user1 = LSTM.
-                    LSTM_user2 = LSTM.
-                    LSTM_user3 = LSTM.
-                    LSTM_user4 = LSTM.
-                    LSTM_user5 = LSTM.
-                observation = [BSmodel0,BSmodel1,BSmodel2,wait_packetSize0,wait_packetSize1,wait_packetSize2,h_15,LSTM_user1,LSTM_user2,LSTM_user3,LSTM_user4,LSTM_user5]
-                #计算reward： reward = 0.6*latency + 0.2*energy_sum +0.2*(-LSTM_user1+LSTM_user2+LSTM_user3+LSTM_user4+LSTM_user5)/10000)
-                #计算latency
-                latency_user0 = sum(arrival_data_matrix0[:-1]-arrival_data_matrix0[:,1])/max(len(user1_data),len(user2_data),len(user3_data),len(user4_data),len(user5_data))
-                latency_user1 = sum(arrival_data_matrix1[:-1] - arrival_data_matrix1[:, 1]) / max(len(user1_data),len(user2_data),len(user3_data),len(user4_data),len(user5_data))
-                latency_user2 = sum(arrival_data_matrix2[:-1] - arrival_data_matrix2[:, 1]) / max(len(user1_data),len(user2_data),len(user3_data),len(user4_data),len(user5_data))
-                lagency =  (latency_user0+latency_user1+latency_user2)/3
-                reward = 0.6*lagency + 0.2*energy_sum + 0.2*((-LSTM_user1+LSTM_user2+LSTM_user3+LSTM_user4+LSTM_user5)/10000)
-        return observation, reward
         # return observation, reward, done, info
+        info = self.get_zero_info()
+        return self.observation_space.sample(), self._compute_reward(), False, info
+
+    def get_zero_info(self):
         info = {
             'time': 0,
             'waiting_package_sizes': np.zeros(3),
             'random_nums': np.zeros(15),
             'user_coming_package_sizes': np.zeros(5),
         }
-        return self.observation_space.sample(), self._compute_reward(), False, info
+        return info
 
     def reset(self):
         self.seed()
@@ -126,7 +66,7 @@ class BaseStationEnv(gym.Env):
             # The user coming package sizes for 5 users.
             'user_coming_package_sizes': np.zeros(5),
         }
-        return self.observation_space.sample(), info
+        return self.observation_space.sample(), self.get_zero_info()
 
     def render(self, mode='human'):
         pass
@@ -137,6 +77,20 @@ class BaseStationEnv(gym.Env):
     def _compute_reward(self):
         return 0
 
+    def get_state_dim(self):
+        state_dim = self.observation_space.shape[0]
+        zero_info = self.get_zero_info()
+        state_dim += 1 # time
+        state_dim += 3 # waiting package sizes for 3 base stations.
+        state_dim += 15 # 15 random numbers.
+        return state_dim
+
+    def get_action_dim(self):
+        return self.action_space.shape[0]
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
 def main():
     env = gym.make('MyBaseStationEnv-v0')
--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-import numpy as np
 import torch
 
 import gym
-
-import Vars
 import ppo
 import base_station_env
 import rl_utils
 import matplotlib.pyplot as plt
 
-# TODO: changed to embedding.
-def _compute_state_dim(obs_space, info):
-    state_dim = obs_space.shape[0]
-    for key, value in obs_space.spaces.items():
-        if key == 'waiting_package_sizes':
-            state_dim += value.shape[0] 
-        elif key == 'random_nums':
-            state_dim += value.shape[0]
-        elif key == 'user_coming_package_sizes':
-            state_dim += value.shape[0]
-        elif key == 'connection_chooses':
-            state_dim += value.shape[0] * value.shape[1]
-        else:
-            raise ValueError('Unknown key: {}'.format(key))
-    return state_dim
-
 def main():
-
     actor_lr = 1e-3
     critic_lr = 1e-2
     num_episodes = 500
@@ -37,39 +17,13 @@ def main():
     eps = 0.2
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
         "cpu")
-    '''
-    我添加的变量
-    1.导入user1-user5的数据集:User1_data,User2_data,User3_data,User4_data,User5_data
-    2.用于存储用户数据的matrix，shape为：len(user_data)*3, 3列分别表示 user_data,user_data到达时间，user_data处理完成时间
-        arrival_data_matrix0,arrival_data_matrix1,arrival_data_matrix2
-     '''
-    # 使用NumPy的genfromtxt函数读取CSV文件
-    #user1
-    User1_data = 'user1_LSTMDate.csv'
-    user1_data = np.genfromtxt(User1_data, delimiter=',')
-    #user2
-    User2_data = 'user1_LSTMDate.csv'
-    user2_data = np.genfromtxt(User2_data, delimiter=',')
-    #user3
-    User3_data = 'user1_LSTMDate.csv'
-    user3_data = np.genfromtxt(User3_data, delimiter=',')
-    #user4
-    User4_data = 'user1_LSTMDate.csv'
-    user4_data = np.genfromtxt(User4_data, delimiter=',')
-    #user5
-    User5_data = 'user1_LSTMDate.csv'
-    user5_data = np.genfromtxt(User5_data, delimiter=',')
-    arrival_data_matrix0 = np.zeros([max(len(user1_data), len(user2_data), len(user3_data), len(user4_data), len(user5_data)),3])
-    arrival_data_matrix1 = np.zeros([max(len(user1_data), len(user2_data), len(user3_data), len(user4_data), len(user5_data)), 3])
-    arrival_data_matrix2 = np.zeros([max(len(user1_data), len(user2_data), len(user3_data), len(user4_data), len(user5_data)), 3])
-
 
     env_name = 'MyBaseStationEnv-v0'
     env = gym.make(env_name)
     env.seed(0)
     torch.manual_seed(0)
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
+    state_dim = env.get_state_dim()
+    action_dim = env.get_action_dim()
 
     agent = ppo.PPO(state_dim, hidden_dim, action_dim, actor_lr, critic_lr, lmbda,
                 epochs, eps, gamma, device)
@@ -88,51 +42,6 @@ def main():
     plt.ylabel('Returns')
     plt.title('PPO on {}'.format(env_name))
     plt.show()
-    '''
-    循环：
-    '''
-    for episode in range(1000):
-        #这些是写在环境中，还是写在main.py函数中
-        done_flag = True #用于判断是否为合法情况
-        time = 0#当前时间，每循环一次time+1，（单位：ms）
-        wait_packetSize0 = 0#初始化三个基站的等待数据
-        wait_packetSize1 = 0
-        wait_packetSize2 = 0
-
-        BSmodel0 = 0#初始化三个BS的 work model
-        BSmodel1 = 0
-        BSmodel2 = 0
-
-        energy_sum = 0#初始化 三个BS的总能耗
-
-        transiotion_time_ratio0 = 0#初始化 三个BS的在1ms内用于转化work model所用时间占总1ms的比例：workmodel转换时间/1ms
-        transiotion_time_ratio1 = 0
-        transiotion_time_ratio2 = 0
-
-        transition_time0 = 0#初始化三个BS work model的转换时间，（单位ms）
-        transition_time1 = 0
-        transition_time2 = 0
-
-        SM_Hold_Time0 = 0#初始化每个基站在某个work model的持续时间
-        SM_Hold_Time1 = 0
-        SM_Hold_Time2 = 0
-
-        LSTM_user1= 0#初始化LSTM
-        LSTM_user2 = 0
-        LSTM_user3 = 0
-        LSTM_user4 = 0
-        LSTM_user5 = 0
-
-
-        while(done_flag):
-            if (Vars.start_trans_no0 == max(len(user1_data),len(user2_data),len(user3_data),len(user4_data),len(user5_data))) and wait_packetSize0 == 0 and wait_packetSize1 == 0 and wait_packetSize2 == 0:
-                done_flag = False
-            elif time >= max(len(user1_data),len(user2_data),len(user3_data),len(user4_data),len(user5_data)) + 800:
-                done_flag = False
-            else:
-
-
-
 
 
 if __name__ == '__main__':
